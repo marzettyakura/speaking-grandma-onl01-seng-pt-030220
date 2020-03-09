efbc13e0b1890f1830aa3428b29eb70a61e27566
pleasant-archiver-6104 <marzettyakura@yahoo.com>
2020-03-09 04:08:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,14 @@
 ## if the phrase is equal to "I LOVE YOU GRANDMA!"
 ## then return "I LOVE YOU TOO PUMPKIN!"
 
+## if the phrase is to equal to an uppercased phrase
+## then return "NO, NOT SINCE 1938!"
+## or else return "HUH?! SPEAK UP, SONNY!"
 def speak_to_grandma(phrase)
-  if phrase=="I LOVE YOU GRANDMA!"
+  if phrase =="I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else return "HUH?! SPEAK UP, SONNY!"
+end
+end
